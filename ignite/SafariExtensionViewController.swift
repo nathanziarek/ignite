0ae1dd2097e8552f6cd7010927fdd40f33b76e33
Nathan Ziarek <nziarek@directs.com>
2016-12-26 20:25:36 -0600

    Initial commit, like 7?

---
+++ ignite/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  ignite
+//
+//  Created by Nate Ziarek on 12/26/16.
+//  Copyright © 2016 Ziarek, LLC. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
