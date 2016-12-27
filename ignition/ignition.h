7896abc851991548e72e339a56681dfead772619
Nathan Ziarek <nziarek@directs.com>
2016-12-26 20:21:40 -0600

    Initial Commit

---
+++ ignition/ignition.h
@@ -0,0 +1,19 @@
+//
+//  ignition.h
+//  ignition
+//
+//  Created by Nate Ziarek on 12/26/16.
+//  Copyright © 2016 Ziarek, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ignition.
+FOUNDATION_EXPORT double ignitionVersionNumber;
+
+//! Project version string for ignition.
+FOUNDATION_EXPORT const unsigned char ignitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ignition/PublicHeader.h>
+
+
